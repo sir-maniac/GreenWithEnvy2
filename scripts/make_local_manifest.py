1ac136009ed4e925e5dbdd5b069c667201a777f0
sir-maniac <608494+sir-maniac@users.noreply.github.com>
2025-10-23 23:43:14 -0600

    Use 'path' for local flatpak install

--- scripts/make_local_manifest.py
+++ scripts/make_local_manifest.py
@@ -6,14 +6,15 @@ from pathlib import Path
 from shutil import copytree
 
 IN_FILE = sys.argv[1]
-GIT_REPO = Path().cwd() / '.git'
+GIT_REPO = Path().cwd()
 OUTPUT = sys.argv[2]
 
 copytree(str(Path(IN_FILE).parent.absolute()), str(Path(OUTPUT).parent.absolute()), dirs_exist_ok=True)
 MANIFEST = json.load(open(IN_FILE, encoding='utf-8'))
 MODULE = MANIFEST['modules'][-1]['sources'][0]
-MODULE['url'] = str(GIT_REPO)
+MODULE['path'] = str(GIT_REPO)
 MODULE['commit'] = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+MODULE.pop('url', None)
 MODULE.pop('branch', None)
 MODULE.pop('tag', None)
 json.dump(MANIFEST, open(OUTPUT, 'w', encoding='utf-8'), indent=2)
